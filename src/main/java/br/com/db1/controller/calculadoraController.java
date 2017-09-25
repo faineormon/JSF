c89a077cc9e064eebd2273f22e7a7f9fb97a9c5a
faine ormon <faine.ormon@gmail.com>
2017-09-25 17:37:28 -0300

    calculadora funcionando somar

---
+++ src/main/java/br/com/db1/controller/calculadoraController.java
@@ -0,0 +1,36 @@
+package br.com.db1.controller;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class calculadoraController {
+	private Double valor1;
+	private Double valor2;
+	private Double resultado;
+	
+	
+	public Double getValor1() {
+		return valor1;
+	}
+	public void setValor1(Double valor1) {
+		this.valor1 = valor1;
+	}
+	public Double getValor2() {
+		return valor2;
+	}
+	public void setValor2(Double valor2) {
+		this.valor2 = valor2;
+	}
+	
+	public void somar() {
+		resultado = valor1 + valor2;
+	}
+	
+	public Double getResultado() {
+		return resultado;
+	}
+	public void setResultado(Double resultado) {
+		this.resultado = resultado;
+	}
+
+}
