bbf1437ed19d52b433295340ac696dc67f0dc009
faine ormon <faine.ormon@gmail.com>
2017-09-26 17:15:53 -0300

    lista com correção até o ex.4

--- src/main/java/br/com/db1/controller/AlfredoController.java
+++ src/main/java/br/com/db1/controller/AlfredoController.java
@@ -6,6 +6,7 @@ import javax.faces.bean.ManagedBean;
 public class AlfredoController {
 	private String nome;
 	private Integer idade;
+	private EstadosSulBrasil estadosSul;
 	
 	
 	public String getNome() {
@@ -25,5 +26,13 @@ public class AlfredoController {
 		setNome("Alfredo Frederico Pereira da Silva II");
 		setIdade(10);
 	}
-
+	
+	public EstadosSulBrasil getEstadosSul() {
+		return estadosSul;
+	}
+	
+	public EstadosSulBrasil[] getListaEstados() {
+		return EstadosSulBrasil.values();
+	}
+	
 }
