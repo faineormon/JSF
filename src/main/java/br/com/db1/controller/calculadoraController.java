5c108ba44d48c9307834bb3ecb9aa2dbda85b900
Francislaine Ortega Monteiro <faine.ormon@gmail.com>
2017-09-26 09:32:54 -0300

    Finalizando calculadora

--- src/main/java/br/com/db1/controller/calculadoraController.java
+++ src/main/java/br/com/db1/controller/calculadoraController.java
@@ -10,6 +10,7 @@ public class calculadoraController {
 	
 	
 	public Double getValor1() {
+
 		return valor1;
 	}
 	public void setValor1(Double valor1) {
@@ -19,18 +20,33 @@ public class calculadoraController {
 		return valor2;
 	}
 	public void setValor2(Double valor2) {
+
 		this.valor2 = valor2;
 	}
-	
-	public void somar() {
-		resultado = valor1 + valor2;
-	}
-	
+
 	public Double getResultado() {
+
 		return resultado;
 	}
 	public void setResultado(Double resultado) {
 		this.resultado = resultado;
+	}
+
+	public void somar() {
+
+		resultado = valor1 + valor2;
+	}
+
+	public void subtrair() {
+		resultado = valor1 - valor2;
+	}
+
+	public void mutiplicar() {
+		resultado = valor1 * valor2;
+	}
+
+	public void dividir() {
+		resultado = valor1 / valor2;
 	}
 
 }
